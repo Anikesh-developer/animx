a56533b43f058d16777fdce336a35215e1e387cc
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-08-09 22:22:01 +0530

    search bar added

--- src/Containers/Navbar/Navbar.jsx
+++ src/Containers/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React , {useState} from 'react' ;
+import {RiMenu3Line , RiCloseLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { IoIosSearch } from "react-icons/io";
+
+const Menu = () => (
+  <>
+          
+            <Link to="/animx/"><p >All Category</p></Link>
+            <Link to='/animx/contactus' ><p className='menu'>Contact Us</p></Link>
+            <Link to="/animx/toprated"><p className='menu' >Top Rated</p></Link>
+          
+  </>
+)
 
 const Navbar = () => {
+
+  const [ toggleMenu , setToggleMenu ] = useState(false);
 
   return (
     <div className='navbar'>
@@ -12,12 +26,25 @@ const Navbar = () => {
           <h1 className='navbar__content-title'>AnimX</h1>
         </div>
         <div className="navbar__content-menuitmes">
-          <ul className='navbar__content-menulist'>
-            <Link to="/animx/"><li >All Category</li></Link>
-            <Link to='/animx/contactus' ><li className='menu'>Contact Us</li></Link>
-            <Link to="/animx/toprated"><li className='menu' >Top Rated</li></Link>
-          </ul>
+          <Menu />
         </div>
+      </div>
+      <div className="navbar__searchbtn">
+          <input className='navbar__searchbtn-input' type='text' placeholder='Type Search'></input>
+          <IoIosSearch />
+        </div>
+      <div className="navbar__menu">
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27}  onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line  color='#fff'  size={27} onClick = {() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='navbar-menu_container scale-up-center'>
+            <div className='navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
